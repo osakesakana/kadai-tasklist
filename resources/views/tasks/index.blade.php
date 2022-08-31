f38ef6c6f857563a2dd42572a22a8b609dfa00ee
tatsushi.inoue <contact@osakesakana.com>
2022-08-31 07:16:20 +0000

    user login

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,31 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <h1>タスク一覧</h1>
-
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>ステータス</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    {{-- タスク詳細ページへのリンク --}}
-                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                    <td>{{ $task->status }}</td>
-                    <td>{{ $task->content }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    
+        <h1>タスク一覧</h1>
+    
+        @if (count($tasks) > 0)
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>ステータス</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                    <tr>
+                        {{-- タスク詳細ページへのリンク --}}
+                        <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
 
     {{-- タスク作成ページへのリンク --}}
     {!! link_to_route('tasks.create', '新規タスクの作成', [], ['class' => 'btn btn-primary']) !!}
-
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Tasklists</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+    
+    @endif
 @endsection
